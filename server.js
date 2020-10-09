beb19068e83e6951a4e649ef3522314ff408b62b
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-10-08 22:39:29 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://password-:password1!@school.9wqbr.mongodb.net/budget?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
